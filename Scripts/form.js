9fe69063bf06f82cbe80a2e32320c076ec2c8a74
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2019-01-10 03:25:56 +0700

    cost done

--- Scripts/form.js
+++ Scripts/form.js
@@ -2,12 +2,23 @@ var quantity;
 var cost;
 
 function inputData(){
-	quantity = document.getElementById("Quantity")
+	quantity = document.getElementById("Quantity").value;
 	cost = 1000 * quantity;
+	localStorage.setItem("cost", cost);
+}
+
+window.onload = function()
+{
+	update();
 }
 
 function update()
 {
-	console.log("a");
-	document.getElementById("totalPayment").innerHTML = "Total Payment : " + cost;
+	console.log(cost);
+	var temp = localStorage.getItem("cost")
+	document.getElementById("totalPayment").innerHTML = "Total Payment : " + temp;
 }
+
+document.addEventListener("click", function(){
+ inputData();
+});
